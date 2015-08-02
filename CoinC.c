a0621a79a0310e57cc4f3f06fb71a34868053f64
webmalf <alain@fontanin.net>
2015-08-02 18:36:24 +0200

    Modification du fichier CoinC: ajout des fonctions Addition et Soustraire.

--- CoinC.c
+++ CoinC.c
@@ -4,3 +4,27 @@ FICHIER		:	COINC.C
 AUTEUR     	:   	WEBMALF
 --------------------------------------------------------------------*/
 
+#include <stdio.h>
+
+int Additionner(int Nbre1, int Nbre2);
+int Soustraire(int Nbre1, int Nbre2);
+
+
+int main(int argc, char *argv[], char *env[])
+{
+    int Nbre1 = 1;
+    int Nbre2 = 2;
+    printf("%d + %d = %d\n", Nbre1, Nbre2, Additionner(Nbre1, Nbre2));
+    printf("%d - %d = %d\n", Nbre1, Nbre2, Soustraire(Nbre1, Nbre2));
+}
+
+
+int Additionner(int Nbre1, int Nbre2)
+{
+    return( Nbre1 + Nbre2);
+}
+
+int Soustraire(int Nbre1, int Nbre2)
+{
+    return( Nbre1 - Nbre2);
+}
