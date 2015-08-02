9d16915ce3692f145435f96edfcd06ebab2180de
webmalf <alain@fontanin.net>
2015-08-02 18:59:08 +0200

    Modification des fichiers CoinC.c et CoinC.h: ajout des fonctions Multiplier et Diviser (CoinC.c) et ajout des décalarations des fonctions (CoinC.h).

--- CoinC.c
+++ CoinC.c
@@ -5,17 +5,18 @@ AUTEUR     	:   	WEBMALF
 --------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include "CoinC.h"
 
-int Additionner(int Nbre1, int Nbre2);
-int Soustraire(int Nbre1, int Nbre2);
 
 
 int main(int argc, char *argv[], char *env[])
 {
-    int Nbre1 = 1;
+    int Nbre1 = 3;
     int Nbre2 = 2;
     printf("%d + %d = %d\n", Nbre1, Nbre2, Additionner(Nbre1, Nbre2));
     printf("%d - %d = %d\n", Nbre1, Nbre2, Soustraire(Nbre1, Nbre2));
+    printf("%d * %d = %d\n", Nbre1, Nbre2, Multiplier(Nbre1, Nbre2));
+    printf("%d / %d = %d\n", Nbre1, Nbre2, Diviser(Nbre1, Nbre2));
 }
 
 
@@ -27,4 +28,14 @@ int Additionner(int Nbre1, int Nbre2)
 int Soustraire(int Nbre1, int Nbre2)
 {
     return( Nbre1 - Nbre2);
+}
+
+int Multiplier(int Nbre1, int Nbre2)
+{
+    return( Nbre1 * Nbre2);
+}
+
+int Diviser(int Nbre1, int Nbre2)
+{
+    return( Nbre1 / Nbre2);
 }
--- CoinC.h
+++ CoinC.h
@@ -4,3 +4,9 @@ FICHIER		:	COINC.H
 AUTEUR     	:   	WEBMALF
 --------------------------------------------------------------------*/
 
+
+int Additionner(int Nbre1, int Nbre2);
+int Soustraire(int Nbre1, int Nbre2);
+int Multiplier(int Nbre1, int Nbre2);
+int Diviser(int Nbre1, int Nbre2);
+
